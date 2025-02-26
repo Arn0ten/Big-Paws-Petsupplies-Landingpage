e34c9cbcbef483b7dbd490edf910f34e20ef9cc0
Arn <a.bautista.129340.tc@umindanao.edu.ph>
2025-02-26 09:28:04 +0800

    New Link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -103,7 +103,7 @@ export default function Header() {
                   ))}
                   <div className="flex flex-col gap-4 mt-4 px-2">
                     <Button variant="outline" asChild className="w-full">
-                      <Link href="https://big-paws-petsupplies.vercel.app/" target="_blank">
+                      <Link href="https://big-paws-petsupplies-webapp.vercel.app/" target="_blank">
                         Log in
                       </Link>
                     </Button>
